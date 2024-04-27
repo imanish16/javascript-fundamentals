618254f80067eb1e14e3202e2adc9cb1fa48444d
Manish Sharma <innosentsharma@gmail.com>
2024-04-27 23:34:23 +0530

    initial commit

---
+++ variables.js
@@ -0,0 +1,50 @@
+// 'use strict';
+/**
+ * Variable is a named storage.
+ * 
+ * Type of variables : var,let,const
+ */
+
+// can use $ and _ . they are considered perfectly fine..
+let $ = 5;
+let _ = 3;
+// console.log($ + _);
+
+
+/**
+ * you can use variable without declaring when use strict is not in use
+ */
+num = 5;
+// console.log(num);
+
+/**
+ * if use strict on you will get reference error.
+ */
+
+/**
+ * const aka constant
+ * 
+ * can be declared only once and can be assigned only once
+ */
+
+
+
+/**
+ * Task by https://javascript.info/variables
+ * 
+ * Declare two variables: admin and name.
+ * Assign the value "John" to name.
+ * Copy the value from name to admin.
+ * Show the value of admin using console.log (must output “John”).
+ */
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+
+console.log(admin);
+
+
+
+
